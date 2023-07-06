b5b73bd95ad8b27e7e6c5b92894fd9bc79e1d2fa
valentinenyongesa <valentinenyongesa94@gmail.com>
2023-07-06 15:57:31 -0700

    Prints binary representation of a number

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,7 +10,21 @@
  */
 void print_binary(unsigned long int n)
 {
-	if (n > 1)
-		print_binary(n / 2);
-	printf("%ld", n % 2);
+	int i, count = 0;
+	unsigned long int current;
+
+	for (i = 63; i >= 0; i--)
+	{
+		current = n >> i;
+
+		if (current & 1)
+		{
+			_putchar('1');
+			count++;
+		}
+		else if (count)
+			_putchar('0');
+	}
+	if (!count)
+		_putchar('0');
 }
