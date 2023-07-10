a73aa211393d886531daad59a50ffa6b4b526d73
valentinenyongesa <valentinenyongesa94@gmail.com>
2023-07-10 04:47:57 -0700

    Create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,4 +9,21 @@
  */
 int create_file(const char *filename, char *text_content)
 {
+	int file, result = 1, count = 0;
 
+	if (!filename)
+		return (-1);
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 600);
+	if (file == -1)
+		return (-1);
+	if (text_content)
+	{
+		while (text_content[count])
+			count++;
+		result = write(file, text_content, count);
+	}
+	if (result == -1)
+		return (-1);
+	close(file);
+	return (1);
+}
