40956714dbb33c7fefa0fc3557c06259df2d7ac3
valentinenyongesa <valentinenyongesa94@gmail.com>
2023-07-10 04:27:30 -0700

    Reads a text file and prints it to the POSIX standard output

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * create_file - function that creates a file
+ * @filename: filename
+ * @text_content: string to write in the fle
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,2 @@
+0x15. C - File I/O
+This project tackles c input output files
