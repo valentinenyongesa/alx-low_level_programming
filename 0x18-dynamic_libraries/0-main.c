fb84c2f3113cb9df78a0d1ddb84f40998a09d55a
valentinenyongesa <valentinenyongesa94@gmail.com>
2023-08-21 02:45:23 -0700

    Create the dynamic library libdynamic.so containing all the functions

---
+++ 0x18-dynamic_libraries/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#define EXIT_SUCCESS 0
+
+/**
+ * main - check the code
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+    printf("%d\n", _strlen("My Dyn Lib"));
+    return (EXIT_SUCCESS);
+}
