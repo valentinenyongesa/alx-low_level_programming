4cfc1cb64aae66ed34b652446900c5d82d17289e
valentinenyongesa <valentinenyongesa94@gmail.com>
2023-08-21 19:44:04 -0700

    Code injection

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+ldd ./gm
+ltrace ./gm
+ltrace ./gm 9 8 10 24 75 9
+strings gm | less
 gcc -fPIC -c test.c
 gcc -shared -o libtest.so test.o
 LD_PRELOAD=/$PWD/libtest.so ./gm 9 8 10 24 75 9
