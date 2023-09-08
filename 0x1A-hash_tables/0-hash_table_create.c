efad37d0d4d2d752b3e52ed1ddff70daf255ead7
valentinenyongesa <valentinenyongesa94@gmail.com>
2023-09-07 18:34:50 -0700

    Creates a hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of array
+ * Return: pointer to new hash table or NULL for error
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new;
+	unsigned long int i;
+
+	if (size == 0)
+		return (NULL);
+
+	new = malloc(sizeof(hash_table_t));
+	if (new == NULL)
+		return (NULL);
+	new->size = size;
+	new->array = malloc(sizeof(hash_node_t *) * size);
+
+	if (new->array == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+		new->array[i] = NULL;
+
+	new->size = size;
+	return (new);
+}
---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,6 @@
+0x1A. C - Hash tables
+This project covers areas in:
+
+1. Hash Tables and Data Structures
+2. Hash Function
+3. Hash Table
